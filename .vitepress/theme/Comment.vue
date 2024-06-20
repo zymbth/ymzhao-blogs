d6dc7cdfd95aea0da4beee591c3cc1b071c41490
ymzhao <ymzhaobth@gmail.com>
2024-06-21 00:39:02 +0800

    添加utterances评论

---
+++ .vitepress/theme/Comment.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://utteranc.es/client.js'
+  script.setAttribute('repo', 'zymbth/ymzhao-blogs')
+  script.setAttribute('issue-term', 'pathname')
+  script.setAttribute('theme', 'preferred-color-scheme')
+  script.setAttribute('label', 'comment')
+  script.async = true
+  script.crossOrigin = 'anonymous'
+  document.querySelector('#comment').appendChild(script)
+})
+</script>
+<template>
+  <div id="comment"></div>
+</template>
---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useData } from 'vitepress'
+import Theme from 'vitepress/theme'
+import Comment from './Comment.vue'
+
+const { Layout } = Theme
+const { theme, frontmatter, page } = useData()
+</script>
+<template>
+  <Layout>
+    <template #doc-footer-before>
+      <Comment :key="page.relativePath" />
+    </template>
+  </Layout>
+</template>
