acd0af7d394a3857194c973b349cfca84da030a1
ymzhao <ymzhaobth@gmail.com>
2024-06-21 14:47:44 +0800

    utterances主题自动切换

--- .vitepress/theme/Comment.vue
+++ .vitepress/theme/Comment.vue
@@ -1,18 +1,42 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, watch } from 'vue'
+import { useData } from 'vitepress'
+
+const { isDark } = useData()
 
 onMounted(() => {
   const script = document.createElement('script')
   script.src = 'https://utteranc.es/client.js'
   script.setAttribute('repo', 'zymbth/ymzhao-blogs')
-  // script.setAttribute('issue-term', 'pathname')
-  script.setAttribute('issue-term', 'title')
-  script.setAttribute('theme', 'github-light')
+  script.setAttribute('issue-term', 'title') // pathname
+  script.setAttribute('theme', isDark.value ? 'github-dark' : 'github-light')
   script.setAttribute('label', 'comment')
   script.async = true
   script.crossOrigin = 'anonymous'
   document.querySelector('#comment').appendChild(script)
+
+  watch(isDark, debounce(updUtteranceTheme, 300))
 })
+
+function updUtteranceTheme(isDark) {
+  const utterances = document.querySelector('#comment iframe')
+  if (!utterances) return
+  utterances.contentWindow.postMessage(
+    { type: 'set-theme', theme: isDark ? 'github-dark' : 'github-light' },
+    'https://utteranc.es'
+  )
+}
+
+function debounce(fn, delay = 300, _this) {
+  let timer
+  return (...args) => {
+    const context = _this
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      context ? fn.apply(context, args) : fn(...args)
+    }, delay)
+  }
+}
 </script>
 <template>
   <div id="comment"></div>
