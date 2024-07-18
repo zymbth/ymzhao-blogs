88aaf9b436044f16714893700db56d95af614669
ymzhao <ymzhaobth@gmail.com>
2024-07-18 23:02:47 +0800

    使用viewerjs替换

--- .vitepress/theme/useViewer.js
+++ .vitepress/theme/useViewer.js
@@ -1,12 +1,13 @@
 import 'viewerjs/dist/viewer.css'
-import VueViewer from 'v-viewer'
+// import VueViewer from 'v-viewer'
+import Viewer from 'viewerjs'
 
 export default app => {
   const defaultOptions = {
     zIndex: 3000,
     inline: false, // Default: false. Enable inline mode.
     button: true, // Show the button on the top-right of the viewer.
-    navbar: true, // Specify the visibility of the navbar.
+    navbar: false, // Specify the visibility of the navbar.
     title: false, // Specify the visibility and the content of the title.
     toolbar: false, // Specify the visibility and layout of the toolbar its buttons.
     tooltip: true, // Show the tooltip with image ratio (percentage) when zooming in or zooming out.
@@ -21,5 +22,26 @@ export default app => {
     url: 'src', // Default: 'src'. Define where to get the original image URL for viewing.
   }
 
-  app.use(VueViewer, { defaultOptions })
+  let gallery
+
+  // app.use(VueViewer, { defaultOptions })
+  app.config.globalProperties.$viewImg = (selector, options) => {
+    if (gallery && !gallery.destroyed) gallery.destroy()
+    if (!(typeof options === 'object')) options = {}
+    try {
+      const el = document.querySelector(selector)
+      if (!el) return
+      gallery = new Viewer(el, {
+        ...defaultOptions,
+        ...options,
+        hidden: function ($event) {
+          try {
+            ;(this.viewer || $event.target.viewer)?.destroy()
+          } catch (error) {}
+        },
+      })
+    } catch (error) {
+      console.error('查看图片失败：', error)
+    }
+  }
 }
