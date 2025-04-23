f5194d95da8fb48df08d4f15baece853e3240140
ymzhao <ymzhaobth@gmail.com>
2025-04-23 18:01:46 +0800

    feat: 新增工具方法

--- .vitepress/theme/utils.js
+++ .vitepress/theme/utils.js
@@ -44,3 +44,61 @@ export function shining(message, color = 'rgb(50, 177, 108)', fontSize = 'xxx-la
   )
   setTimeout(() => i.remove(), duration)
 }
+
+export function message(message, type = 'info', duration = 3000) {
+  if (!message) return
+  const color = { info: '#909399', primary: '#409EFF', success: '#67C23A', warning: '#E6A23C', error: '#F56C6C' }[type] || '#909399'
+  const wrapEl = document.createElement('div')
+  wrapEl.style.cssText = `
+    position: fixed;
+    top: 20px;
+    left: 50%;
+    max-width: 80%;
+    padding: 6px 24px 6px 10px;
+    color: ${color};
+    font-size: 14px;
+    border-radius: 4px;
+    border: 1px solid ${color};
+    background-color: ${color}33;
+    opacity: 0
+    transform: translateX(-50%);
+    z-index: 2002;
+  `
+  const i = document.createElement('span')
+  i.textContent = message
+  wrapEl.appendChild(i)
+  document.body.appendChild(wrapEl)
+  wrapEl.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 100, fill: 'forwards' })
+
+  const removeFn = () => {
+    wrapEl.animate(
+      [
+        { top: '20px', opacity: 1 },
+        { top: '-20px', opacity: 0 }
+      ],
+      { duration: 200, fill: 'forwards' }
+    )
+    setTimeout(() => wrapEl.remove(), 200)
+  }
+  let timer = setTimeout(removeFn, duration)
+  wrapEl.onmouseenter = () => clearTimeout(timer)
+  wrapEl.onmouseleave = () => {
+    timer = setTimeout(removeFn, duration)
+  }
+
+  const closeEl = document.createElement('span')
+  closeEl.textContent = '+'
+  closeEl.style.cssText = `
+    position: absolute;
+    top: 50%;
+    right: 8px;
+    font-size: 20px;
+    transform: translateY(-50%) rotate(45deg);
+    cursor: pointer;
+  `
+  closeEl.onclick = () => {
+    clearTimeout(timer)
+    removeFn()
+  }
+  wrapEl.appendChild(closeEl)
+}
