f7cd11f344f20f9758753bb08d7229a4bb0e3367
ymzhao <ymzhaobth@gmail.com>
2025-09-22 15:48:32 +0800

    feat: 修复主题切换，添加动画时的闪烁bug

--- .vitepress/theme/Layouts/Typography/components/SwitchAppearance.vue
+++ .vitepress/theme/Layouts/Typography/components/SwitchAppearance.vue
@@ -1,45 +1,43 @@
 <script setup>
-// import { useData } from 'vitepress'
+import { useData } from 'vitepress'
 import VPSwitchAppearance from 'vitepress/dist/client/theme-default/components/VPSwitchAppearance.vue'
-// import { nextTick, provide } from 'vue'
+import { nextTick, provide } from 'vue'
 
-// const { isDark } = useData()
+const { isDark } = useData()
 
-/* 主题切换 */
+const enableTransitions = () =>
+  'startViewTransition' in document
+  && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
 
-// const enableTransitions = () =>
-//   !import.meta.env.SSR
-//   && 'startViewTransition' in document
-//   && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value
+    return
+  }
 
-// provide('toggle-appearance', async ({ clientX: x, clientY: y }) => {
-//   if (!enableTransitions()) {
-//     isDark.value = !isDark.value
-//     return
-//   }
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y)
+    )}px at ${x}px ${y}px)`
+  ]
 
-//   const clipPath = [
-//     `circle(0px at ${x}px ${y}px)`,
-//     `circle(${Math.hypot(
-//       Math.max(x, innerWidth - x),
-//       Math.max(y, innerHeight - y)
-//     )}px at ${x}px ${y}px)`,
-//   ]
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
 
-//   await document.startViewTransition(async () => {
-//     isDark.value = !isDark.value
-//     await nextTick()
-//   }).ready
-
-//   document.documentElement.animate(
-//     { clipPath: isDark.value ? clipPath.reverse() : clipPath },
-//     {
-//       duration: 500,
-//       easing: 'ease-in',
-//       pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
-//     }
-//   )
-// })
+  document.documentElement.animate(
+    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+    {
+      duration: 300,
+      easing: 'ease-in',
+      fill: 'forwards',
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+    }
+  )
+})
 </script>
 
 <template>
@@ -47,6 +45,22 @@ import VPSwitchAppearance from 'vitepress/dist/client/theme-default/components/V
 </template>
 
 <style>
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+
 .VPSwitchAppearance {
   width: 22px !important;
 }
