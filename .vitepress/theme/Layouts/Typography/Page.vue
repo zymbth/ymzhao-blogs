65a0dac12b397f11f2bbf6958d0738beebf121eb
ymzhao <ymzhaobth@gmail.com>
2025-09-23 02:04:58 +0800

    test: 添加页面访问测试代码

--- .vitepress/theme/Layouts/Typography/Page.vue
+++ .vitepress/theme/Layouts/Typography/Page.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onContentUpdated, useData, useRoute } from 'vitepress'
-import { computed } from 'vue'
+import { computed/* , onMounted, ref */ } from 'vue'
 import Comment from '../components/Comment.vue'
 // import VPDocFooterLastUpdated from 'vitepress/dist/client/theme-default/components/VPDocFooterLastUpdated.vue'
 import DocFooterCreated from './components/DocFooterCreated.vue'
@@ -14,6 +14,26 @@ const pageName = computed(() => route.path.replace(/[./]+/g, '_').replace(/_html
 onContentUpdated(() => {
   setTimeout(jumpToHash, 500)
 })
+
+// const count = ref('')
+// onMounted(() => {
+//   // 获取当前页面的路径，例如 "/posts/abc.html"
+//   const path = window.location.pathname
+
+//   // 使用 encodeURIComponent 对路径进行编码，防止路径中包含特殊字符 (&, ?, /) 导致解析错误
+//   const encodedPath = encodeURIComponent(path)
+
+//   // 通过 'path' 查询参数将编码后的路径传递给 Worker
+//   fetch(`https://api-site-statistics.ymzhao.work/?path=${encodedPath}`)
+//     .then(res => res.json())
+//     .then((res) => {
+//       console.log('Fetched data', res)
+//     })
+//     .catch((err) => {
+//       console.warn('Fetch error')
+//       console.error(err)
+//     })
+// })
 </script>
 
 <template>
